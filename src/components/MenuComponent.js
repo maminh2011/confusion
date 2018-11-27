74a272074b088dec9c09a7206866cabf8ad1b0db
Trung Nguyen <jindothap2011@gmail.com>
2018-11-26 23:46:41 -0500

    Splitting Presentational and Container Components by creating Main Component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,44 +1,23 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
 class Menu extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        }
+        console.log('Menu Component constructor invoked');    
     }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
-
-    renderDish(dish) {
-        if (dish !=null) {
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
+    
+    componentDidMount() {
+            console.log('MenuComponent componentDidMount invoked');
         }
-        else {
-            return(
-                <div></div>
-            );
-        }
-    }
 
     render(){
-
+        console.log('Menu Component render invoked')
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={()=> this.props.onClick(dish.id)}>
                             <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -52,9 +31,6 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
